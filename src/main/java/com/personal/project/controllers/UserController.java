66fb2861a7dd07fe6ae2a6326495a024711ebf93
amandabacelli <amanda.sted@gmail.com>
2021-02-01 21:01:14 -0300

    create user save method

--- src/main/java/com/personal/project/controllers/UserController.java
+++ src/main/java/com/personal/project/controllers/UserController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +32,14 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        user = service.save(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(user.getId()).toUri(); //salva no header o location do recurso (endpoint getId)
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
--- src/main/java/com/personal/project/services/UserService.java
+++ src/main/java/com/personal/project/services/UserService.java
@@ -21,4 +21,8 @@ public class UserService {
     public User findById(Long id) {
         return repository.findById(id).orElse(null);
     }
+
+    public User save(User user) {
+        return repository.save(user);
+    }
 }
